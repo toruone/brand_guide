7a1342c1c566c003c8d66eebc836456f82048443
toru <toru0722@gmail.com>
2020-04-08 01:23:15 -0400

    add_infosite_CRUD

---
+++ app/controllers/info_sites_controller.rb
@@ -0,0 +1,51 @@
+class InfoSitesController < ApplicationController
+  before_action :set_brand, only: [:show, :edit, :update, :destroy]
+  def show
+  end
+
+  def index
+    @info_sites = InfoSite.all
+  end
+
+  def create
+    @info_site = InfoSite.new(info_site_params)
+    if @info_site.save
+      redirect_to info_sites_path
+    else
+      render "new"
+    end
+  end
+
+  def new
+    @info_site = InfoSite.new
+  end
+
+  def edit
+  end
+
+  def destroy
+    @info_site.destroy
+    redirect_to info_sites_path
+  end
+
+  def update
+    if @info_site.update(info_site_params)
+      redirect_to info_sites_path(@info_site)
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def set_brand
+    @info_site = InfoSite.find(params[:id])
+  end
+
+  def info_site_params
+    params[:info_site].permit(
+      :name,
+      :url,
+    )
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "brands#index"
   resources :brands
+  resources :info_sites
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
