5a4a6a92028868431c3130dce1bdfa758b47e5c2
toru <toru0722@gmail.com>
2020-04-08 11:48:02 -0400

    fix templete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,7 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require rails-ujs
+//= require owl-carousel/owl.carousel.min
+//= require smoothscroll
+//= require scrollreveal.min
+//= require script
