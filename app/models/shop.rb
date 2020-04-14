eaef650308489b1b000e13255bf487f7a2ac1458
toru <toru0722@gmail.com>
2020-04-14 09:14:08 -0400

    fix shop show

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,7 @@
 class Shop < ApplicationRecord
   mount_uploader :image, ShopImageUploader
-
+  mount_uploader :image1, BrandImageUploader
+  mount_uploader :image2, BrandImageUploader
+  mount_uploader :image3, BrandImageUploader
   belongs_to :brand
 end
