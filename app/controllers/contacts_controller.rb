acbfa8414371cd377755d24783c84fdb61fe2559
toru <toru0722@gmail.com>
2020-05-01 12:32:53 -0400

    ajdust inquiry page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,10 +6,22 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      redirect_to contact_path
+      contact = Contact.find(params[:id]) #contact_mailer.rbの引数を指定
+      contact.update(contact_params)
+      user = contact.user
+      ContactMailer.send_when_admin_reply(user, contact).deliver
+      redirect_to new_contact_path
     else
       render "new"
     end
+  end
+
+  def contact_params
+    params[:contact].permit(
+      :user_name,
+      :email,
+      :inquiry,
+    )
   end
 
 end
