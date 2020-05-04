c340c75e0e575853818c48366a4e456fd39a1ebd
toru <toru0722@gmail.com>
2020-05-04 15:27:28 -0400

    adjust mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,10 +6,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      contact = Contact.find(params[:id]) #contact_mailer.rbの引数を指定
-      contact.update(contact_params)
-      user = contact.user
-      ContactMailer.send_when_admin_reply(user, contact).deliver
+      ContactMailer.send_when_user_contact(@contact).deliver
       redirect_to new_contact_path
     else
       render "new"
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,7 @@
 class ContactMailer < ApplicationMailer
 
-  def send_when_user_contact(user, contact) #メソッドに対して引数を設定
-    @user = user #ユーザー情報
-    @answer = contact.reply_text #返信内容
-    mail to: user.email, subject: '【World Brands】Thank you for your inquiry'
+  def send_when_user_contact(contact) #メソッドに対して引数を設定
+    @contact = contact
+    mail to: contact.email, subject: '【World Brands】Thank you for your inquiry'
   end
 end
