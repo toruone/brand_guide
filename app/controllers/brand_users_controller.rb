a8c884651fae3e9317bbbf073810982ad79af2ae
toru <toru0722@gmail.com>
2020-05-12 00:53:39 -0400

    adjust info_user & brand_user controller

--- app/controllers/brand_users_controller.rb
+++ app/controllers/brand_users_controller.rb
@@ -1,14 +1,14 @@
 class BrandUsersController < ApplicationController
   def create
-    brand_user = current_user.brand_users.build(brand_id: params[:brand_id])
+    brand_user = current_user.brand_users.find_or_initialize_by(brand_id: params[:brand_id])
     brand_user.save
-    redirect_to brand_path(params[:brand_id]), notice: "お気に入りに追加しました。" 
+    redirect_to brand_path(params[:brand_id]), notice: "Add to MyPage Succssfully!"
   end
 
   def destroy
     brand_user = BrandUser.find_by(brand_id: params[:brand_id], user_id: current_user.id)
     brand_user.destroy
-    redirect_to brand_path(params[:brand_id]), notice: "お気に入りから削除しました。"
+    redirect_to brand_path(params[:brand_id]), notice: "おDelete done"
   end
 
 
