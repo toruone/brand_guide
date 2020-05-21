fd1034d9b3fdea9db6b56650d834213135b075d2
toru <toru0722@gmail.com>
2020-05-21 11:14:58 -0400

    fix info_users controller

--- app/controllers/info_users_controller.rb
+++ app/controllers/info_users_controller.rb
@@ -6,7 +6,7 @@ class InfoUsersController < ApplicationController
   end
 
   def destroy
-    info_user = InfoUser.find_by(info_site_id: params[:info_site_id], user_id: current_user.id)
+    info_user = InfoUser.find_by!(info_site_id: params[:info_site_id], user_id: current_user.id)
     info_user.destroy
     redirect_to info_sites_path, notice: "Delete done"
   end
