3afdc7f93abe4d97356b5f848074f15f2feeafd6
toru <toru0722@gmail.com>
2020-08-11 23:29:33 -0400

    delete confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :brand_users, dependent: :destroy
   has_many :brands, through: :brand_users
 
